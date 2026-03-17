0c886be49791feedfc8129ddb5deebca7c53c301
ivanishvilil774-dot <ivanishvilil774@gmail.com>
2026-03-17 15:52:16 +0100

    Movie API

--- Utils/writefile.js
+++ Utils/writefile.js
@@ -1,12 +1,11 @@
-// utils/writeFile.js
 const fs = require("fs/promises");
 
 const writeFile = async (file, data) => {
     try {
-        await fs.writeFile(file, JSON.stringify(data)); // null- no formatting, 2 for pretty formatting
+        await fs.writeFile(file, JSON.stringify(data, null, 2)); // pretty-print with 2 spaces
         return "File written successfully";
-    } catch (e) {
-        return e;
+    } catch (err) {
+        throw new Error(`Failed to write file: ${err.message}`);
     }
 };
 
